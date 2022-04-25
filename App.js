0d9579aede8232c5e3e4051daf19313970e0c389
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2022-04-24 21:40:28 -0400

    Initial files created and basic function tested on ios Expo Go

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to ReCRM.
+        Login : Signup
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'crimson',
     alignItems: 'center',
     justifyContent: 'center',
   },
